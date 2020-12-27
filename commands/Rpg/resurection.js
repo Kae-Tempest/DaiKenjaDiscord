75af1d60636760446bf1e3d5bea99e964c23d1c3
xLeiry <kyllianlegrand78@gmail.com>
2020-12-27 17:35:47 +0100

    rez update

--- commands/Rpg/resurection.js
+++ commands/Rpg/resurection.js
@@ -20,12 +20,26 @@ module.exports.run = async (client, message) => {
         }
     }
 
-    //let rez = [{lvlmin:0,lvlmax:10,cost:250}];
-    //$res = array( 0 => array(10,250), 10 => array(20, 1000), 20 , 30);
-
     ressurection(0, 10, 250);
     ressurection(10, 20, 1000);
     ressurection(20, 30, 5000);
+    ressurection(30, 40, 8000);
+    ressurection(40, 50, 10000);
+    ressurection(50, 60, 12000);
+    ressurection(60, 70, 15000);
+    ressurection(70, 80, 18000);
+    ressurection(80, 90, 20000);
+    ressurection(90, 100, 50000);
+    ressurection(100, 200, 100000);
+    ressurection(200, 300, 200000);
+    ressurection(300, 400, 300000);
+    ressurection(400, 500, 400000);
+    ressurection(500, 600, 500000);
+    ressurection(600, 700, 600000);
+    ressurection(700, 800, 700000);
+    ressurection(800, 900, 800000);
+    ressurection(900, 999, 900000);
+    ressurection(999, 1000, 1000000);
 };
 
 module.exports.help = {
