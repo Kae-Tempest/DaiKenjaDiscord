7ebdff55310e9ab5e1d864cca98180863dc8a5d8
xLeiry <kyllianlegrand78@gmail.com>
2021-01-16 15:41:42 +0100

    litle refacto

--- function/rpg/delete.js
+++ function/rpg/delete.js
@@ -1,13 +1,13 @@
-let b = 5;
 const deleting = async (message, i) => {
+    let b;
     let a = 0;
-    if(i === 1) b = 5;
+    if (i === 1) b = 5;
     if (i === 5) {
         a = 4;
         b += 5
         await message.channel.bulkDelete(a);
     }
-    while(b === i) {
+    while (b === i) {
         if (i === b) {
             a = 5;
             await message.channel.bulkDelete(a);
