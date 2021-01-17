ec006ab8db026fa97f76bfe52aea5c95f995dff6
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-01-17 13:35:59 +0100

    augmentation du CD des battles

--- commands/Rpg/battle.js
+++ commands/Rpg/battle.js
@@ -16,7 +16,7 @@ module.exports.help = {
     aliases: ["battle","Battle","BATTLE"],
     category: 'rpg',
     description: 'Bataille avec un monstre',
-    cd: 5,
+    cd: 20,
     usage: "<enemy_name>",
     isUserAdmin: false,
     permissions: false,
