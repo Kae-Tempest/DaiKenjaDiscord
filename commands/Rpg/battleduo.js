26b6c04e6e9598881b7b6b1448fa0f4fae45fa9b
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-02-09 21:48:24 +0100

    delete console.log

--- commands/Rpg/battleduo.js
+++ commands/Rpg/battleduo.js
@@ -10,7 +10,6 @@ module.exports.run = async (client, message, args) => {
     const player2 = mentionedUser;
     if (player2.username !== player.teams) return message.replay("tu n'es pas en teams avec lui !")
     const q = args.slice(1).join(" ")
-    console.log(q)
     const position = hostile.map(e => e.name).indexOf(capitalize(q));
     if (q && position === -1) return message.reply ('Ce monstre n\'existe pas !')
     const monster = hostile[position];
