258e177ebcd5f74ae740f0e1b1603356d41bb258
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-02-09 20:20:24 +0100

    fix comma

--- function/rpg/createItems.js
+++ function/rpg/createItems.js
@@ -183,7 +183,7 @@ const items = (client) => {
 
     createItems("God armor", "heroique", "toute classe", "63", "armor", "Armure de dieux", 500000, 0, 0, 0, 0, 800, 400);
 
-    createItems("Demoniac armor", "heroique", "toute classe", "64", "armor", "Armure du roi démon", 750000, 0, 0, 0, 0, 1200, 600, 1);
+    createItems("Demoniac armor", "heroique", "toute classe", "64", "armor", "Armure du roi démon", 750000, 0, 0, 0, 0, 1200, 600);
 
     //ACCESSOIRES
 
@@ -281,7 +281,7 @@ const items = (client) => {
 
     createItems("Aegis", "drop", "toute classe", null, "Oh", "Bouclier divin", 10000000, 0, 0, 0, 0, 10000, 10000, 1);
 
-    createItems("Divino démoniac armor", "drop", "toute classe", null, "armor", "Armure Divino Démoniaque", 10000000, 0, 10000, 10000, 0, 30000, 30000);
+    createItems("Divino démoniac armor", "drop", "toute classe", null, "armor", "Armure Divino Démoniaque", 10000000, 0, 10000, 10000, 0, 30000, 30000, 1);
 
     createItems("Divino démoniac broach", "drop", "toute classe", null, "broach", "Broche Divino Démoniaque", 15000000, 10000, 10000, 10000, 10000, 10000, 10000);
 
