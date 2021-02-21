a12d0a81c14dd666a663b5e173c9ab408abd852f
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-02-21 14:14:15 +0100

    begin refacto

--- commands/Economy/weapon.js
+++ commands/Economy/weapon.js
@@ -1,27 +1,25 @@
 const {MessageEmbed} = require('discord.js');
-
-module.exports.run = async (client, message) => {
-
-    const items = [];
-    const shop = require('../../assets/shop/weapon.json');
-    const embed = new MessageEmbed()
-        .setTitle('Bienvenue dans notre shop !')
-        .setColor('RANDOM')
+module.exports = {
+    run: async message => {
+        const items = [];
+        const shop = require('../../assets/shop/weapon.json');
+        const embed = new MessageEmbed()
+            .setTitle('Bienvenue dans notre shop !')
+            .setColor('RANDOM')
         shop.map(e => items.push(`${e.label}.**${e.name}** *(${e.cost})*`));
         embed.setDescription(`Voici les differentes armes disponible :\n${items.map(item => `${item}`).join("\n")}`)
         embed.setFooter('pensez à regarder le détail des objets ! ');
         message.channel.send(embed)
+    }, help: {
+        name: "weapon",
+        aliases: ["weapon", "Weapon", "WEAPON"],
+        category: 'economy',
+        description: "affiche les armes",
+        cd: 3,
+        usage: "",
+        isUserAdmin: false,
+        permission: false,
+        args: false,
+        profile: false
     }
-
-module.exports.help = {
-    name: "weapon",
-    aliases: ["weapon","Weapon","WEAPON"],
-    category: 'economy',
-    description: "affiche les armes",
-    cd: 3,
-    usage: "",
-    isUserAdmin: false,
-    permission: false,
-    args: false,
-    profile: false
-};
+}
