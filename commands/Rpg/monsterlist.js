fa899730959bab4c16392056792d27415a705a23
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-02-23 14:40:14 +0100

    feat: add show on monsterlist

--- commands/Rpg/monsterlist.js
+++ commands/Rpg/monsterlist.js
@@ -1,16 +1,29 @@
 const {MessageEmbed} = require('discord.js');
+const { capitalize } = require('../../function/other/string');
 module.exports = {
-    run: message => {
+    run: (client, message, args) => {
     const monsters = [];
     const hostile = require('../../assets/npc/hostile.json');
     hostile.map(e => monsters.push(`**${e.name} (${e.category})** *Difficulty:${e.difficulty}*`));
-
+    if(args[0] === "show"){
+        const q = args.slice(1).join(" ")
+        const position = hostile.map(n => n.name).indexOf(capitalize(q))
+        const hostiles = hostile[position]
+        const showEmbed = new MessageEmbed()
+        .setTitle(`${hostiles.name} `)
+        .setColor('RANDOM')
+        .addField("Type:", `${hostiles.type}`)
+        .addField("stats:", `${Object.entries(hostiles.stats).map(([key, value]) => `${capitalize(key)}: ${value}`).join(' | ')}`)
+        .addField("stats:", `${Object.entries(hostiles.attribut).map(([key, value]) => `${capitalize(key)}: ${value}`).join(' | ')}`)
+    message.channel.send(showEmbed);
+    } else {
     const embed = new MessageEmbed()
         .setTitle('Liste des monstres !')
         .setColor('RANDOM')
         .setDescription(`Voici les differents monstre combattable :\n${monsters.map(monster => `${monster}`).join("\n")}`)
         .setFooter(`Kono sekai no kamigami ni yoru to`)
     message.channel.send(embed)
+    }
     }, help: {
         name: 'monsterlist',
         aliases: ["monsterlist", "ml", "MONSTERLIST","ML","Monsterlist","Ml"],
