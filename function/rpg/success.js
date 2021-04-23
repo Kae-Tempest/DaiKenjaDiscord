86cbc1716186aa3b9bcb306b4e3e290591371673
Kae-Tempest <Katsuhiko.tempest@gmail.com>
2021-04-23 18:23:15 +0200

    Update success.js

--- function/rpg/success.js
+++ function/rpg/success.js
@@ -16,143 +16,603 @@ const success = (client, message, hostile, player) => {
             break;
         case 'Goblin' :
             client.updateUserInfo(message.member, {"users.$.Goblin" : player.Goblin += 1});
+            switch(player.Goblin){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Wildboar' :
             client.updateUserInfo(message.member, {"users.$.Wildboar" : player.Wildboar += 1});
+            switch(player.Wildboar){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Cave Spider' :
             client.updateUserInfo(message.member, {"users.$.Cave_Spider" : player.Cave_Spider += 1});
+            switch(player.Cave_Spider){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Wolf' :
             client.updateUserInfo(message.member, {"users.$.Wolf" : player.Wolf += 1});
+            switch(player.Wolf){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Skeleton' :
             client.updateUserInfo(message.member, {"users.$.Skeleton" : player.Skeleton += 1});
+            switch(player.Skeleton) {
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Shadow' :
             client.updateUserInfo(message.member, {"users.$.Shadow" : player.Shadow += 1});
+            switch(player.Shadow){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Ogre' :
             client.updateUserInfo(message.member, {"users.$.Ogre" : player.Ogre += 1});
+            switch(player.Ogre){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Giant Mantis' :
             client.updateUserInfo(message.member, {"users.$.Giant_Mantis" : player.Giant_Mantis += 1});
+            switch(player.Giant_Mantis){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Orc' :
             client.updateUserInfo(message.member, {"users.$.Orc" : player.Orc += 1});
+            switch(player.Orc){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Phantomtooth' :
             client.updateUserInfo(message.member, {"users.$.Phantomtooth" : player.Phantomtooth += 1});
+            switch(player.Phantomtooth){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Vaporhag' :
             client.updateUserInfo(message.member, {"users.$.Vaporhag" : player.Vaporhag += 1});
+            switch(player.Vaporhag){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Gloomfang' :
             client.updateUserInfo(message.member, {"users.$.Gloomfang" : player.Gloomfang += 1});
+            switch(player.Gloomfang){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Taintscreamer' :
             client.updateUserInfo(message.member, {"users.$.Taintscreamer" : player.Taintscreamer += 1});
+            switch(player.Taintscreamer){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Cryptsoul' :
             client.updateUserInfo(message.member, {"users.$.Cryptsoul" : player.Cryptsoul += 1});
+            switch(player.Cryptsoul){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Mimic' :
             client.updateUserInfo(message.member, {"users.$.Mimic" : player.Mimic += 1});
+            switch(player.Mimic){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Zombie' :
             client.updateUserInfo(message.member, {"users.$.Zombie" : player.Zombie += 1});
+            switch(player.Zombie){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Becale' :
             client.updateUserInfo(message.member, {"users.$.Becale" : player.Becale += 1});
+            switch(player.Becale){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Sopurenne' :
             client.updateUserInfo(message.member, {"users.$.Sopurenne" : player.Sopurenne += 1});
+            switch(player.Sopurenne){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Dark timilo' :
             client.updateUserInfo(message.member, {"users.$.Dark_timilo" : player.Dark_timilo += 1});
+            switch(player.Dark_timilo){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'King Slime' :
             client.updateUserInfo(message.member, {"users.$.King_Slime" : player.King_Slime += 1});
+            switch(player.King_Slime){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Moonlight cat' :
             client.updateUserInfo(message.member, {"users.$.Moonlight_cat" : player.Moonlight_cat += 1});
+            switch(player.Moonlight_cat){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Orc Disaster' :
             client.updateUserInfo(message.member, {"users.$.Orc_Disaster" : player.Orc_Disaster += 1});
+            switch(player.Orc_Disaster){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Elyon' :
             client.updateUserInfo(message.member, {"users.$.Goblin" : player.Goblin += 1});
+            switch(player.Elyon){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Dracula' :
             client.updateUserInfo(message.member, {"users.$.Elyon" : player.Elyon += 1});
+            switch(player.Dracula) {
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Bowser' :
             client.updateUserInfo(message.member, {"users.$.Bowser" : player.Bowser += 1});
+            switch(player.Bowser){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Fake Kami' :
             client.updateUserInfo(message.member, {"users.$.Fake_Kami" : player.Fake_Kami += 1});
+            switch(player.Fake_Kami){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Shogun' :
             client.updateUserInfo(message.member, {"users.$.Shogun" : player.Shogun += 1});
+            switch(player.Shogun){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'The Gleam Eyes' :
             client.updateUserInfo(message.member, {"users.$.The_Gleam_Eyes" : player.The_Gleam_Eyes += 1});
+            switch(player.The_Gleam_Eyes){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Hellwraith' :
             client.updateUserInfo(message.member, {"users.$.Hellwraith" : player.Hellwraith += 1});
+            switch(player.Hellwraith){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
     }
     switch(name) {
         case 'Vampmask' :
             client.updateUserInfo(message.member, {"users.$.Vampmask" : player.Vampmask += 1});
+            switch(player.Vampmask){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Dreadfang' :
             client.updateUserInfo(message.member, {"users.$.Dreadfang" : player.Dreadfang += 1});
+            switch(player.Dradfang){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Hellstep' :
             client.updateUserInfo(message.member, {"users.$.Hellstep" : player.Hellstep += 1});
+            switch(player.Hellstep) {
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Wispling' :
             client.updateUserInfo(message.member, {"users.$.Wispling" : player.Wispling += 1});
+            switch(player.Wispling) {
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Kae' :
             client.updateUserInfo(message.member, {"users.$.Kae" : player.Kae += 1});
+            switch(player.Kae){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Charybdis' :
             client.updateUserInfo(message.member, {"users.$.Charybdis" : player.Charybdis += 1});
+            switch(player.Charybdis){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Yuusha' :
             client.updateUserInfo(message.member, {"users.$.Yuusha" : player.Yuusha += 1});
+            switch(player.Yuusha){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Demon Lord' :
             client.updateUserInfo(message.member, {"users.$.Demon_Lord" : player.Demon_Lord += 1});
+            switch(player.Demon_Lord){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Maho' :
             client.updateUserInfo(message.member, {"users.$.Maho" : player.Maho += 1});
+            switch(player.Maho){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Norico' :
             client.updateUserInfo(message.member, {"users.$.Norico" : player.Norico += 1});
+            switch(player.Norico){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Thor' :
             client.updateUserInfo(message.member, {"users.$.Thor" : player.Thor += 1});
+            switch(player.Thor){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Odin' :
             client.updateUserInfo(message.member, {"users.$.Odin" : player.Odin += 1});
+            switch(player.Odin){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Reaper' :
             client.updateUserInfo(message.member, {"users.$.Reaper" : player.Reaper += 1});
+            switch(player.Reaper){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Rimuru' :
             client.updateUserInfo(message.member, {"users.$.Rimuru" : player.Rimuru += 1});
+            switch(player.Rimuru){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Skarab' :
             client.updateUserInfo(message.member, {"users.$.Skarab" : player.Skarab += 1});
+            switch(player.Skarab){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Naminoe' :
             client.updateUserInfo(message.member, {"users.$.Naminoe" : player.Naminoe += 1});
+            switch(player.Naminoe){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
         case 'Kami' :
             client.updateUserInfo(message.member, {"users.$.Kami" : player.Kami += 1});
+            switch(player.Kami){
+                case 1 :
+                    break;
+                case 10 :
+                    break;
+                case 50 :
+                    break;
+                case 1000 :
+                    break;
+            }
             break;
     }
 }
